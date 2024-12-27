a1f017a47f1a76051956a7f842cbe44915eb74f9
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-12-27 17:16:31 +0100

    fix deprication warning 'initial_properties'
    WARNING[Server]: Reading initial object properties directly from an
    entity definition is deprecated, move it to the 'initial_properties'
    table instead. (Property 'static_save' in entity
    'spacecannon:energycube_<colour>')

--- cannon.lua
+++ cannon.lua
@@ -35,11 +35,11 @@ local register_spacecannon = function(def)
 				entity_texture
 			},
 			collisionbox = {-0.25,-0.25,-0.25, 0.25,0.25,0.25},
-			physical = false
+			physical = false,
+			static_save = false,
 		},
 		timer = 0,
 		lifetime = 0,
-		static_save = false,
 		penetrated = 0,
 
 		on_step = function(self, dtime)
